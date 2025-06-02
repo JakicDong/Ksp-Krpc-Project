95b10e4dafd37111c557c0bed9ae782239b1fe2f
MrGEFORCE <2584356276@qq.com>
2025-06-02 15:25:14 +0800

    try fix ap

--- starShipRecovery.py
+++ starShipRecovery.py
@@ -12,7 +12,7 @@ conn = krpc.connect(
 space_center = conn.space_center
 vessel = space_center.active_vessel
 
-USING_AP = False
+USING_AP = True
 root = vessel.parts.root
 stack = [(root, 0)]
 KS25Engine_parts = []
@@ -21,7 +21,7 @@ while stack:
     if part.title == "S3 KS-25“矢量”液体燃料引擎":
         KS25Engine_parts.append(part)
     for child in part.children:
-        stack.append((child, depth+1))
+        stack.append((child, depth + 1))
 
 
 class PIDUsingV:
@@ -57,14 +57,22 @@ def set_engine_gimbal(lists: list, gim: float):
         item.engine.gimbal_limit = gim
 
 
-def reference_rotate():
-    lon = vessel.flight().longitude
-    ref = space_center.ReferenceFrame.create_relative(vessel.orbit.body.reference_frame, rotation=(
-        0, np.sin(-lon / 2. * np.pi / 180), 0, np.cos(-lon / 2. * np.pi / 180)))
-    return ref
+def create_relative_reference_frame():
+    body_frame = vessel.orbit.body.reference_frame  # 地固系
+    lon = vessel.flight(body_frame).longitude
+    lat = vessel.flight(body_frame).latitude
+    # 绕y轴旋转-lon度
+    temp1 = space_center.ReferenceFrame.create_relative(body_frame, rotation=(0., np.sin(-lon / 2. * np.pi / 180), 0., np.cos(-lon / 2. * np.pi / 180)))
+    # 绕z轴旋转lat度
+    temp2 = space_center.ReferenceFrame.create_relative(temp1, rotation=(0., 0., np.sin(lat / 2. * np.pi / 180), np.cos(lat / 2. * np.pi / 180)))
+    # 沿x轴平移
+    height = vessel.orbit.body.surface_height(lat, lon) + vessel.orbit.body.equatorial_radius
+    target_frame = space_center.ReferenceFrame.create_relative(temp2, position=(height, 0., 0.))
+    return target_frame
 
 
-def steering(pitch_bias: float, ref: space_center.ReferenceFrame):
+def steering(pitch_bias: float):
+    ref = create_relative_reference_frame()
     v = vessel.flight(ref).velocity
     retro_steering = v / np.linalg.norm(v)
 
@@ -76,13 +84,34 @@ def steering(pitch_bias: float, ref: space_center.ReferenceFrame):
     vessel.control.roll = roll_controller(vessel.flight(ref).roll)
 
 
-def steering_ap(pitch_bias: float, ref: space_center.ReferenceFrame):
-    vessel.auto_pilot.reference_frame = ref
-    v = vessel.flight(ref).velocity
-    retro_steering = v / np.linalg.norm(v)
-    target_pitch = - np.arcsin(retro_steering[0]) / np.pi * 180 + pitch_bias
-    target_heading = - np.arctan(retro_steering[1] / retro_steering[2]) / np.pi * 180 + 270
-    vessel.auto_pilot.target_pitch_and_heading(target_pitch, target_heading)
+count = 0
+
+
+def calculate_angle(vec1: np.ndarray, vec2: np.ndarray) -> float:
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+    cos_theta = dot_product / (norm1 * norm2)
+    angle_radians = np.arccos(np.clip(cos_theta, -1.0, 1.0))
+    return angle_radians
+
+
+def steering_ap(pitch_bias: float):
+    vessel.auto_pilot.reference_frame = create_relative_reference_frame()
+    bias_radius = pitch_bias * np.pi / 180
+    rot_mat = np.array([[np.cos(bias_radius), 0, -np.sin(bias_radius)], [0, 1, 0], [np.sin(bias_radius), 0, np.cos(bias_radius)]])
+    retro = -np.array(vessel.flight(vessel.auto_pilot.reference_frame).velocity)
+    retro_with_bias = rot_mat @ retro
+    vessel.auto_pilot.target_direction = retro_with_bias
+    global count
+    count += 1
+    if count > 10:
+        print("angle diff {:.3f}".format(calculate_angle(retro, retro_with_bias) / np.pi * 180))
+        print("pitch_pid_gains", vessel.auto_pilot.pitch_pid_gains, "  error {:.2f}".format(vessel.auto_pilot.pitch_error))
+        print("yaw_pid_gains", vessel.auto_pilot.yaw_pid_gains, "  error {:.2f}".format(vessel.auto_pilot.heading_error))
+        print("roll_pid_gains", vessel.auto_pilot.roll_pid_gains, "  error {:.2f}".format(vessel.auto_pilot.roll_error), "  ctrl {:.2f}".format(vessel.control.roll))
+        print()
+        count = 0
 
 
 print("auto landing started")
@@ -93,8 +122,15 @@ vessel.control.toggle_action_group(8)  # grid fin deploy
 
 if USING_AP:
     vessel.auto_pilot.engage()
-    vessel.auto_pilot.reference_frame = reference_rotate()
+    vessel.auto_pilot.reference_frame = create_relative_reference_frame()
     vessel.auto_pilot.target_roll = 0
+    vessel.auto_pilot.pitch_pid_gains = (10, 1, 4)
+    vessel.auto_pilot.yaw_pid_gains = (10, 1, 4)
+    vessel.auto_pilot.roll_pid_gains = (0.2, 0, 1)
+    vessel.auto_pilot.auto_tune = False
+    pitch_controller = None
+    heading_controller = None
+    roll_controller = None
 else:
     pitch_controller = PID(Kp=0.1, Ki=0.01, Kd=0.4, output_limits=(-1, 1), differential_on_measurement=False)
     heading_controller = PID(Kp=0.1, Ki=0.01, Kd=0.4, output_limits=(-1, 1), differential_on_measurement=False)
@@ -106,13 +142,12 @@ final_throttle.init(kp=0.01, ki=0, kd=0.03, upper=0.8, lower=0)
 set_engine_gimbal(KS25Engine_parts, 0.2)
 
 while vessel.flight().mean_altitude > 1000:
-    ref = reference_rotate()
     if USING_AP:
-        steering_ap(-10, ref)
+        steering_ap(-10)
     else:
-        steering(-10, ref)
+        steering(-10)
 
-    if vessel.flight().mean_altitude < 26000 and vessel.flight(ref).speed > 1300:
+    if vessel.flight().mean_altitude < 26000 and vessel.flight(create_relative_reference_frame()).speed > 1300:
         vessel.control.throttle = 0.33
     else:
         vessel.control.throttle = 0
@@ -124,18 +159,17 @@ if not USING_AP:
     heading_controller.tunings = (0.4, 0.01, 1)
 bGearDeploy = True
 while vessel.flight().surface_altitude > 14:
-    ref = reference_rotate()
-    vessel.control.throttle = final_throttle.update(13 - vessel.flight(ref).surface_altitude, -vessel.flight(ref).vertical_speed)
-    if np.linalg.norm(vessel.flight(ref).velocity[1:]) > 50:
+    vessel.control.throttle = final_throttle.update(13 - vessel.flight(vessel.orbit.body.reference_frame).surface_altitude, -vessel.flight(vessel.orbit.body.reference_frame).vertical_speed)
+    if np.linalg.norm(vessel.flight(vessel.orbit.body.reference_frame).velocity[1:]) > 50:
         if USING_AP:
-            steering_ap(-10, ref)
+            steering_ap(-10)
         else:
-            steering(-10, ref)
+            steering(-10)
     else:
         if USING_AP:
-            steering_ap(0, ref)
+            steering_ap(0)
         else:
-            steering(0, ref)
+            steering(0)
 
     if bGearDeploy and vessel.flight().surface_altitude < 250:
         bGearDeploy = False
